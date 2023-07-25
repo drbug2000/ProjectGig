4a759186a1c0433ee8150c888419530e39db9ece
bucheasal <leejunwoo9@gmail.com>
2023-07-25 17:00:11 +0900

    Revert "fish image"
    This reverts commit dfdbea7bec91622f80754eca93e54bea5d87c8b9.
    물고기 기본 상태 AI

---
+++ Assets/Resource/SeaCreature/fpatrol.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class fpatrol : MonoBehaviour
+{
+    public float speed;
+    public float startWaitTime;
+    private float waitTime;
+    public Transform moveSpot;
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+    private void Start()
+    {
+        waitTime = startWaitTime;
+        moveSpot.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+    }
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpot.position, speed *
+        Time.deltaTime);
+        if (Vector2.Distance(transform.position, moveSpot.position) < 0.2)
+        {
+            if (waitTime <= 0)
+            {
+                moveSpot.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+        
+
+    }
+}
+
