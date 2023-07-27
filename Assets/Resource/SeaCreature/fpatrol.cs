085b77d441f10ea6c4faf0005480d9f4eab33600
drbug2000 <drbug2000@naver.com>
2023-07-27 16:10:52 +0900

    fpatrol update
    refactoring code
    add flee code. now fish move back to player

--- Assets/Resource/SeaCreature/fpatrol.cs
+++ Assets/Resource/SeaCreature/fpatrol.cs
@@ -18,18 +18,20 @@ public class fpatrol : MonoBehaviour
 
     Vector2 dir;
 
+
     private void Start()
     {
         Renderer = GetComponent<SpriteRenderer>();
         fishRigidbody = GetComponent<Rigidbody2D>();
         setNewSpot();
+        speed = 10;
         
     }
     private void Update()
     {
         //transform.position = Vector2.MoveTowards(transform.position, moveSpot.position, speed *Time.deltaTime);
-        
-        if (Vector2.Distance(transform.position, moveSpot.position) < 1)
+        //MoveSpot에 도달하면 waitTime만큼 대기
+        if (Vector2.Distance(transform.position, moveSpot.position) < 2)
         {
             if (waitTime <= 0)
             {
@@ -40,31 +42,45 @@ public class fpatrol : MonoBehaviour
                 waitTime -= Time.deltaTime;
             }
         }
-
+        //속도가 1 이하로 떨어지면 방향 재설정 이후 다시 가속
         if(fishRigidbody.velocity.magnitude < 1)
         {
             dir = moveSpot.position - transform.position;
-            fishRigidbody.AddForce(dir * (3));
+            fishRigidbody.AddForce(dir * (speed/dir.magnitude));
+
+            if (moveSpot.position.x > transform.position.x)
+            {
+                Renderer.flipX = false;
+            }
+            else
+            {
+                Renderer.flipX = true;
+            }
         }
     }
     public void setNewSpot()
     {
         waitTime = startWaitTime;
         moveSpot.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
-        dir = moveSpot.position - transform.position;
-        if(moveSpot.position.x > transform.position.x)
-        {
-                Renderer.flipX = false;
-            }
-        else
-        {
-                Renderer.flipX = true ;
-            }
-
-        fishRigidbody.AddForce(dir*(3));
+        Debug.Log(moveSpot.position);
     }
-        
 
-    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            dir = transform.position - other.transform.position; 
+            fishRigidbody.AddForce(dir * (speed*10) / dir.magnitude);
+            Debug.Log("PP");
+            /*
+        ContactPoint2D contact = other.contacts[0];
+        Vector2 normal = -1 * contact.normal;
+        fishRigidbody.AddForce(normal * (speed*3 / dir.magnitude));
+            */
+        }
+    }
+
+
+
 }
 
