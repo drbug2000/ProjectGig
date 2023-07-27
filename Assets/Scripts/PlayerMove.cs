31ee6db25c56511ea64bdbab9d706a201313f851
TowJ <codjeongsh02@gmail.com>
2023-07-27 22:19:23 +0900

    playermove, playerswim, playerAttack 추가 및 수정했습니다.
    playerswim으로 한가지 방법이 될 수 있을 것 같습니다.
    
    attack은 함수만 추가하였을 뿐 어떠한 모션을 추가하지 않았습니다.

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,46 +4,75 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public AudioClip deathClip; // 사망시 재생할 오디오 클립
+    // public AudioClip deathClip; // 사망시 재생할 오디오 클립
     public float jumpForce = 700f; // 점프 힘
+    public float swimForce = 350f; // 헤엄 힘
     public float Speed = 5f;
-    private int jumpCount = 0; // 누적 점프 횟수
+    protected int jumpCount = 0; // 누적 점프 횟수
    
-    private bool isGrounded = false; // 바닥에 닿았는지 나타냄
-    private bool isDead = false; // 사망 상태
+    protected bool isGrounded = false; // 바닥에 닿았는지 나타냄 점프할 때 쓰는 변수
+    // private bool isDead = false; // 사망 상태
+    public bool onboard; // 갑판 위에 있는지
 
-    private Rigidbody2D playerRigidbody; // 사용할 리지드바디 컴포넌트
-    private SpriteRenderer playerSpriteRenderer;
-    private Animator animator; // 사용할 애니메이터 컴포넌트
-    private AudioSource playerAudio; // 사용할 오디오 소스 컴포넌트
+    protected Rigidbody2D playerRigidbody; // 사용할 리지드바디 컴포넌트
+    // private SpriteRenderer playerSpriteRenderer;
+    // private Animator animator; // 사용할 애니메이터 컴포넌트
+    // private AudioSource playerAudio; // 사용할 오디오 소스 컴포넌트
 
-    private bool CurrentSpeed = false;
+    protected bool CurrentSpeed = false; // 이건 뭔지 모르겠습니다...?
 
-    private PlayerController playerInput;
-    private Animator playerAnimator;
+    public PlayerController playerInput;
+    // private Animator playerAnimator;
+
+    public GameObject inventoryparents;
     
     // Start is called before the first frame update
-    private void Start()
+    void Start()
     {
         // 초기화
         playerInput = GetComponent<PlayerController>();
         playerRigidbody = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-        playerAudio = GetComponent<AudioSource>();
-        playerSpriteRenderer = GetComponent<SpriteRenderer>();
+        // animator = GetComponent<Animator>();
+        // playerAudio = GetComponent<AudioSource>();
+        // playerSpriteRenderer = GetComponent<SpriteRenderer>();
+        onboard = true;
+        inventoryparents.SetActive(false);
 
     }
 
     // Update is called once per frame
-    private void Update()
+    void Update()
     {
         // 사용자 입력을 감지하고 점프하는 처리
-        if (isDead)
+        // if (isDead)
+        // {
+        //     return;
+            // }
+        if (onboard == true)
         {
-            return;
+            playerRigidbody.gravityScale = 1; // 배 위에 있을 때 중력 1
+            playerRigidbody.drag = 1;
+            playerwalk();
+        }
+        else
+        {
+            playerRigidbody.gravityScale = 0; // 물 속에 있을 때 중력 0
+            playerRigidbody.drag = 1.5f;
+            playerswim();
         }
 
-        if(Input.GetKeyDown(KeyCode.UpArrow) && jumpCount<2)
+        Attack();
+
+        
+        // animator.SetBool("Grounded",isGrounded);
+        // animator.SetBool("CurrentSpeed", CurrentSpeed);
+    }
+    
+    // 플레이어가 땅 위에서 움직이는 것에 관한 함수
+    public void playerwalk()
+    {
+        // 점프에 관한 내용입니다.
+        if(Input.GetButtonDown("Jump") && jumpCount < 2)
         {
             jumpCount++;
 
@@ -51,62 +80,82 @@ public class PlayerMove : MonoBehaviour
 
             playerRigidbody.AddForce(new Vector2(0, jumpForce));
 
-            playerAudio.Play();
+            // playerAudio.Play();
 
-        }else if(Input.GetKeyUp(KeyCode.UpArrow) && playerRigidbody.velocity.y > 0 )
+        }
+        
+        else if(Input.GetButtonDown("Jump") && playerRigidbody.velocity.y > 0 )
         {
             playerRigidbody.velocity = playerRigidbody.velocity * 0.5f;
         }
+
+        // x축 방향으로 움직일 때 사용되는 내용입니다.
+        transform.Translate(Vector3.right * Speed * playerInput.move_x * Time.deltaTime);
         
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.Translate(Vector3.right * Speed * Time.deltaTime);
-            playerSpriteRenderer.flipX = false;
-            CurrentSpeed = true;
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.Translate(Vector3.left * Speed * Time.deltaTime);
-            playerSpriteRenderer.flipX = true;
-            CurrentSpeed = true;
-        }
-        else
-        {
-            CurrentSpeed = false;
-        }
 
-
-        // x = Input.GetAxis("Horizontal");
-        /*
-        Debug.Log(playerRigidbody.velocity.x);
-        if (playerRigidbody.velocity.x == 0)
-        {
-            CurrentSpeed = false;
-        }else
-        {
-            CurrentSpeed = true;
-        }
-        */
-
-        
-        animator.SetBool("Grounded",isGrounded);
-        animator.SetBool("CurrentSpeed", CurrentSpeed);
+        // current speed가 필요가 없다면 위의 내용과 아래의 내용은 같습니다.
+        // if (playerInput.move_x > 0)
+        // {
+        //     transform.Translate(Vector3.right * Speed * playerInput.move_x * Time.deltaTime);
+        //     // playerSpriteRenderer.flipX = false;
+        //     CurrentSpeed = true;
+        // }
+        // else if (playerInput.move_x < 0)
+        // {
+        //     transform.Translate(Vector3.right * Speed * playerInput.move_x * Time.deltaTime);
+        //     // playerSpriteRenderer.flipX = true;
+        //     CurrentSpeed = true;
+        // }
+        // else if (playerInput.move_x == 0 || Input.GetKeyUp("A") || Input.GetKeyUp("D"))
+        // {
+        //     transform.Translate(Vector3.zero);
+        //     CurrentSpeed = false;
+        // }
     }
 
-    private void OnTriggerEnter2D(Collider2D other) {
-       // 트리거 콜라이더를 가진 장애물과의 충돌을 감지
-       if(other.tag == "Dead" && !isDead)
+    // 플레이어가 물 속에 있을 때 움직이는 것에 관한 함수입니다.
+    public void playerswim()
+    {
+        playerRigidbody.AddForce(Vector3.right * playerInput.move_x);
+        playerRigidbody.AddForce(Vector3.up * playerInput.move_y);
+
+        // // x축 방향으로 움직일 때
+        // if (playerInput.move_x > 0)
+        // {
+        //     playerRigidbody.AddForce(Vector3.right * playerInput.move_x);
+        //     // playerSpriteRenderer.flipX = false;
+        // }
+        // else if (playerInput.move_x < 0)
+        // {
+        //     playerRigidbody.AddForce(Vector3.right * playerInput.move_x);
+        //     // playerSpriteRenderer.flipX = true;
+        // }
+        // else
+        // {
+        //     transform.Translate(Vector3.zero);
+        // }
+
+        // // y축 방향으로 움직일 때 
+        // if (playerInput.move_y > 0)
+        // {
+        //     playerRigidbody.AddForce(Vector3.up * playerInput.move_y);
+        // }
+        // else if (playerInput.move_y < 0)
+        // {
+        //     playerRigidbody.AddForce(Vector3.up * playerInput.move_y);
+        // }
+        // else
+        // {
+        //     transform.Translate(Vector3.zero);
+        // }
+    }
+
+    public void Attack()
+    {
+        if (playerInput.fire == true)
         {
-            GameManager.instance.GetDamage(5);
-
+            // Animator.SetBool("?", true);
         }
-
-       /*
-       if(other.tag == "Coin" && !isDead)
-        {
-
-        }
-       */
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
