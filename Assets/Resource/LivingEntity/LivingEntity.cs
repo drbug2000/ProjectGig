5f96ed59acae10368de8d7a70642958cf62921d6
yeseul1231 <yesel1231@konkuk.ac.kr>
2023-08-22 14:18:04 +0900

    fish health 스크립트와 애니메이션
    1. 큰물고기2에 fish health 스크립트 추가
    2. 통나무 뗏목 추가
    3. 바다 이미지와 배경 스크롤 추가
    (부자연스럽게 연결되므로 이미지 수정 필요)
    4. 큰물고기 1, 2에 애니메이션 추가

--- Assets/Resource/LivingEntity/LivingEntity.cs
+++ Assets/Resource/LivingEntity/LivingEntity.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 
 public class LivingEntity : MonoBehaviour, IDamageable
 {
-    public float startingHealth = 100f; // 시작 체력
+    public float startingHealth; // 시작 체력
     public float health { get; protected set; } // 현재 체력
     public bool dead { get; protected set; } // 사망 상태
     public event Action onDeath; // 사망시 발동할 이벤트
