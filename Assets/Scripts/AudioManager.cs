a6825f0e869f0b4aaf7a36f528159f70a0f49b9a
TowJ <codjeongsh02@gmail.com>
2023-08-22 17:03:35 +0900

    게임 시작 버튼 이미지 추가하였습니다.

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
 
 public class AudioManager : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/SoundSetting.cs
+++ Assets/Scripts/SoundSetting.cs
@@ -1,19 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class SoundSetting : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public AudioMixer masterMixer;
+    public Slider audioSlider;
 
-    // Update is called once per frame
-    void Update()
+    public void AudioController()
     {
-        
+        float sound = audioSlider.value;
+
+        if (sound == -40f)
+        {
+            masterMixer.SetFloat("BGM", -80);
+        }
+        else
+        {
+            masterMixer.SetFloat("BGM", sound);
+        }
     }
 
     public void ToggleAudioVolume()
