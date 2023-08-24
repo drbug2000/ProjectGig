d6142432f42968d112d9d14764e5b44bc6e7dbd6
yeseul1231 <yesel1231@konkuk.ac.kr>
2023-08-24 15:25:05 +0900

    sprites 폴더와 애니메이션 추가
    기존 sprite 파일을 sprites 폴더로 묶음
    큰물고기2 애니메이션 적용
    상어 애니메이션 추가

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -12,13 +12,20 @@ public class PlayerHealth : LivingEntity
     //6. 바다와 육지를 판별하여, hp가 회복 또는 감소한다.
     //7. 상점에서 플레이어의 hp를 업그레이드할 수 있다.
 
+    private int maxHp = 100;
+    private int seaDamage = 5;
+    private int restorevalue = 10; 
+
     public SpriteRenderer flshSpriteRenderer;
+    public PlayerMove playerMove;
+   
 
     private void Awake()
     {
-        //컴포넌트 할당: 피격 시에 색 변환
+        //컴포넌트 할당: 피격 시에 색 변환을 위함
         flshSpriteRenderer = GetComponent<SpriteRenderer>();
-
+        //컴포넌트 할당: 바다와 육지 판별 위함
+        playerMove = GetComponent<PlayerMove>();
     }
 
     protected override void OnEnable()
@@ -27,6 +34,47 @@ public class PlayerHealth : LivingEntity
         // LivingEntity의 OnEnable() 실행 (상태 초기화)
         base.OnEnable();
 
+    }
+
+    private void Update()
+    {
+        //바다, 육지 확인하여 피를 깎는다
+        if (playerMove.onboard != true)
+        {
+            OnDamage(seaDamage, new Vector3(0, 0, 0), new Vector3(0, 0, 0));
+        }
+        else
+        {
+
+            if (dead == false)
+            {
+                if (health >= maxHp)
+                {
+                    health = maxHp;
+                }
+                else
+                {
+                    Restoring(restorevalue);            
+                }
+            }
+            
+        }
+
+
+    }
+    //천천히 회복하는 코루틴 처리
+    private IEnumerator Restoring(int value)
+    {
+        RestoreHealth(value);
+        yield return new WaitForSeconds(4f);
+    }
+
+
+    //회복 처리
+    public override void RestoreHealth(float newHealth)
+    {
+        // LivingEntity의 RestoreHealth() 실행 (체력 증가)
+        base.RestoreHealth(newHealth);
     }
 
     // 데미지 처리
@@ -45,9 +93,10 @@ public class PlayerHealth : LivingEntity
     private IEnumerator DamageEffect()
     {
         flshSpriteRenderer.material.color = new Color(1f, 168 / 255f, 168 / 255f);
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(2f);
         flshSpriteRenderer.material.color = new Color(1f, 1f, 1f);
     }
+    
 
 
     public override void Die()
